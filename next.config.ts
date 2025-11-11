82f0d13483fdf4c6d56a424e60b6e990d49462ef
llt <418797644@qq.com>
2025-11-12 01:06:15 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*',
+        basePath: false,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
