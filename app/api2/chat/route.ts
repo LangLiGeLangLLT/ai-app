5784a1536e574b6a66d64d50bbfc3e07c70530c3
llt <418797644@qq.com>
2025-11-26 00:36:05 +0800

    update

--- app/api2/chat/route.ts
+++ app/api2/chat/route.ts
@@ -2,13 +2,14 @@ import {
   streamText,
   UIMessage,
   convertToModelMessages,
-  createUIMessageStreamResponse,
-  createUIMessageStream,
   tool,
   stepCountIs,
 } from 'ai'
 import { qwen } from '@/lib/ai/qwen'
-import { experimental_createMCPClient as createMCPClient } from '@ai-sdk/mcp'
+import {
+  experimental_createMCPClient as createMCPClient,
+  experimental_MCPClient as MCPClient,
+} from '@ai-sdk/mcp'
 import prompt from './prompt.md'
 import z from 'zod'
 import { retrieve } from '@/lib/actions/retrieve'
@@ -26,85 +27,68 @@ export async function POST(req: Request) {
     webSearch: boolean
   } = await req.json()
 
-  return createUIMessageStreamResponse({
-    stream: createUIMessageStream({
-      async execute({ writer }) {
-        const modelMessages = convertToModelMessages(messages)
+  let sseClient: MCPClient | null = null
+  let sseClientTools = null
 
-        if (webSearch) {
-          try {
-            const sseClient = await createMCPClient({
-              transport: {
-                type: 'sse',
-                url: 'https://dashscope.aliyuncs.com/api/v1/mcps/WebSearch/sse',
-                headers: {
-                  Authorization: `Bearer ${process.env.DASHSCOPE_API_KEY}`,
-                },
-              },
-            })
-            const sseClientTools = await sseClient.tools({
-              schemas: {
-                bailian_web_search: {
-                  description:
-                    'Search can be used to query information such as encyclopedia knowledge, current news, weather, etc.',
-                  inputSchema: z.object({
-                    query: z
-                      .string()
-                      .describe('user query in the format of string'),
-                    count: z
-                      .number()
-                      .optional()
-                      .describe('number of search results'),
-                  }),
-                },
-              },
-            })
-            const { response } = streamText({
-              model: qwen(model),
-              messages: modelMessages,
-              tools: sseClientTools,
-              onFinish: async () => {
-                await sseClient.close()
-              },
-              onError: async () => {
-                await sseClient.close()
-              },
-            })
-
-            modelMessages.push(...(await response).messages)
-          } catch {}
-        }
-
-        const result = streamText({
-          model: qwen(model),
-          messages: modelMessages,
-          system: prompt,
-          prepareStep: async ({ messages }) => {
-            if (messages.length > 20) {
-              return {
-                messages: messages.slice(-10),
-              }
-            }
-            return {}
+  try {
+    if (webSearch) {
+      sseClient = await createMCPClient({
+        transport: {
+          type: 'sse',
+          url: 'https://dashscope.aliyuncs.com/api/v1/mcps/WebSearch/sse',
+          headers: {
+            Authorization: `Bearer ${process.env.DASHSCOPE_API_KEY}`,
           },
-          stopWhen: stepCountIs(5),
-          tools: {
-            retrieve: tool({
-              description: `get information from your knowledge base to answer questions.`,
-              inputSchema: z.object({
-                query: z.string().describe(`the users query`),
-              }),
-              execute: async ({ query }) => retrieve(query),
+        },
+      })
+      sseClientTools = await sseClient.tools({
+        schemas: {
+          bailian_web_search: {
+            description:
+              'Search can be used to query information such as encyclopedia knowledge, current news, weather, etc.',
+            inputSchema: z.object({
+              query: z.string().describe('user query in the format of string'),
+              count: z.number().optional().describe('number of search results'),
             }),
           },
-        })
-        writer.merge(
-          result.toUIMessageStream({
-            sendSources: true,
-            sendReasoning: true,
-          })
-        )
-      },
-    }),
+        },
+      })
+    }
+  } catch {}
+
+  const result = streamText({
+    model: qwen(model),
+    messages: convertToModelMessages(messages),
+    system: prompt,
+    prepareStep: async ({ messages }) => {
+      if (messages.length > 20) {
+        return {
+          messages: messages.slice(-10),
+        }
+      }
+      return {}
+    },
+    stopWhen: stepCountIs(5),
+    tools: {
+      ...(webSearch ? sseClientTools : {}),
+      retrieve: tool({
+        description: `get information from your knowledge base to answer questions.`,
+        inputSchema: z.object({
+          query: z.string().describe(`the users query`),
+        }),
+        execute: async ({ query }) => retrieve(query),
+      }),
+    },
+    onFinish: async () => {
+      await sseClient?.close()
+    },
+    onError: async () => {
+      await sseClient?.close()
+    },
+  })
+
+  return result.toUIMessageStreamResponse({
+    sendSources: true,
+    sendReasoning: true,
   })
 }
